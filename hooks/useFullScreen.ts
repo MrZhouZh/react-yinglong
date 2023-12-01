ea8f6b2eb99712838407b73335b58a84d63d8e0d
zhouzhikai <zhouzhikai@smm.cn>
2023-12-01 18:53:50 +0800

    feat: add fullscreen

---
+++ hooks/useFullScreen.ts
@@ -0,0 +1,81 @@
+import { RefObject, useState } from "react"
+import screenfull from 'screenfull'
+import { noop, off, on } from "~/misc/util";
+import useIsomorphicLayoutEffect from "./useIsomorphicLayoutEffect";
+
+export interface FullscreenOptions {
+  video?: RefObject<
+    HTMLVideoElement & {
+      webkitEnterFullscreen?: () => void;
+      webkitExitFullscreen: () => void
+    }
+  >;
+  onClose?: (error?: Error | unknown) => void;
+}
+
+const useFullscreen = (
+  ref: RefObject<Element>,
+  enabled: boolean,
+  options: FullscreenOptions = {}
+) => {
+  const { video, onClose = noop } = options
+  const [isFullscreen, setIsFullscreen] = useState(enabled)
+
+  useIsomorphicLayoutEffect(() => {
+    if (!enabled) return
+    if (!ref.current) return
+
+    const onWebkitEndFullscreen = () => {
+      if (video?.current) {
+        off(video.current, 'webkitendfullscreen', onWebkitEndFullscreen)
+      }
+      onClose()
+    }
+
+    const onChange = () => {
+      if (screenfull.isEnabled) {
+        const isScreenfullFullscreen = screenfull.isFullscreen
+        setIsFullscreen(isScreenfullFullscreen)
+
+        if (!isScreenfullFullscreen) {
+          onClose()
+        }
+      }
+    }
+
+    if (screenfull.isEnabled) {
+      try {
+        screenfull.request(ref.current)
+        setIsFullscreen(true)
+      } catch (error) {
+        onClose(error)
+        setIsFullscreen(false)
+      }
+      screenfull.on('change', onChange)
+    } else if (video && video.current && video.current.webkitEnterFullscreen) {
+      video.current.webkitEnterFullscreen()
+      on(video.current, 'webkitendfullscreen', onWebkitEndFullscreen)
+      setIsFullscreen(true)
+    } else {
+      onClose()
+      setIsFullscreen(false)
+    }
+
+    return () => {
+      setIsFullscreen(false)
+      if (screenfull.isEnabled) {
+        try {
+          screenfull.off('change', onChange)
+          screenfull.exit()
+        } catch {}
+      } else if (video && video.current && video.current.webkitExitFullscreen) {
+        off(video.current, 'webkitendfullscreen', onWebkitEndFullscreen)
+        video.current.webkitExitFullscreen()
+      }
+    }
+  }, [enabled, video, ref])
+
+  return isFullscreen
+}
+
+export default useFullscreen
--- misc/util.ts
+++ misc/util.ts
@@ -18,6 +18,10 @@ export function off<T extends Window | Document | HTMLElement | EventTarget>(
   }
 }
 
+export function getPixelRatio() {
+  return window.devicePixelRatio || 1
+}
+
 export const isBrowser = typeof window !== 'undefined'
 
 export const isNavigator = typeof navigator !== 'undefined'
